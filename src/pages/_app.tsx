f3df103de2525e02da20a4f9b0c7abb923d71ce1
Wafflus <wafflusdev@example.com>
2022-12-11 21:49:55 +0000

    Add roboto font to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { Roboto } from '@next/font/google';
+import type { AppProps } from 'next/app';
+
+const roboto = Roboto({
+  style: ['italic', 'normal'],
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --roboto-font: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default MyApp;
