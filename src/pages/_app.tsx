8c56e82ea6f9ee124f6884038c104a056907caed
Wafflus <wafflusdev@example.com>
2022-12-12 15:53:19 +0000

    Add random fishes menu item to application main component menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import { AiFillPicture } from 'react-icons/ai';
 import { BiLinkExternal } from 'react-icons/bi';
 import { Link, Menu, MenuArea, MenuItem } from '@components/global';
-import { MdOutgoingMail } from 'react-icons/md';
+import { MdFactCheck, MdOutgoingMail } from 'react-icons/md';
 import { Roboto } from '@next/font/google';
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
@@ -36,6 +36,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Menu title='Random API' defaultActiveItem={pathname.slice(1)} separateItemsChildren={extraMenuOptions}>
           <MenuArea title='Navigation' >
             <MenuItem id='random-pictures' href='/random-pictures'><><AiFillPicture/> <span>Random Pictures</span></></MenuItem>
+            <MenuItem id='random-fishes' href='/random-fishes'><><MdFactCheck/> <span>Random Fishes</span></></MenuItem>
           </MenuArea>
         </Menu>
 
