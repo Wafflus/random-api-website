789cb116984f8fb42f5ae1b98f23d5e206493009
Wafflus <wafflusdev@example.com>
2022-12-12 14:43:57 +0000

    Add link component

--- src/common/components/global/data/index.ts
+++ src/common/components/global/data/index.ts
@@ -1,2 +1,3 @@
 export { ApiFooter } from './api-footer';
 export { Image } from './image';
+export { Link } from './link';
--- src/common/components/global/index.ts
+++ src/common/components/global/index.ts
@@ -1,4 +1,4 @@
-export { ApiFooter, Image } from './data';
+export { ApiFooter, Image, Link } from './data';
 export { Button } from './submit';
 export { Footer, Menu } from './layout';
 export { GetData, MenuArea, MenuItem, Range } from './options';
