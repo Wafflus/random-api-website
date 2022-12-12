c1097fddbe8ec80002b6ef04804ef78b755382cb
Wafflus <wafflusdev@example.com>
2022-12-12 14:43:27 +0000

    Add images paths and main page redirect to next configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'random.dog'
+      }, {
+        protocol: 'https',
+        hostname: 'cdn.shibe.online'
+      }, {
+        protocol: 'https',
+        hostname: 'randomfox.ca'
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/random-pictures',
+        permanent: true,
+      },
+    ]
+  },
 }
