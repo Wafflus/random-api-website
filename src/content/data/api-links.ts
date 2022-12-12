af900f442a04d212d048f7f61d174f9ac6e950d2
Wafflus <wafflusdev@example.com>
2022-12-12 14:11:27 +0000

    Add api links data content

---
+++ src/content/data/api-links.ts
@@ -0,0 +1,9 @@
+export const imageApiLinks: string[] = [
+    'https://random.dog/',
+    'https://shibe.online/',
+    'https://randomfox.ca/'
+]
+
+export const fishApiLinks: string[] = [
+    'https://www.fishwatch.gov/'
+];
---
+++ src/content/index.ts
@@ -0,0 +1,1 @@
+export { imageApiLinks, fishApiLinks } from './data';
