f3ef52d120a0fe4941a9a31b536a9187e769a791
Wafflus <wafflusdev@example.com>
2022-12-12 10:49:03 +0000

    Add radio group component

---
+++ src/common/components/random-pictures/options/radio-group/stories/RadioGroup.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { RadioButton } from '../../radio-button';
+import { RadioGroup } from '..';
+import React, { ReactElement } from 'react';
+
+export default {
+  title: 'Components/Radio Group',
+  component: RadioGroup,
+  argTypes: {
+    activeItem: { options: ['One', 'Two', 'Three'], control: { type: 'radio' } },
+    children: { table: { disable: true } }
+  }
+} as ComponentMeta<typeof RadioGroup>;
+
+const Template: ComponentStory<typeof RadioGroup> = (args) => <RadioGroup {...args} />;
+
+const children: ReactElement[] = [
+    <RadioButton label='One' value='One' />,
+    <RadioButton label='Two' value='Two' />,
+    <RadioButton label='Three' value='Three' />
+];
+
+export const Checked = Template.bind({});
+export const Unchecked = Template.bind({});
+
+Checked.args = {
+  children,
+  activeItem: 'One',
+  groupName: 'radioGroupChecked',
+};
+
+Unchecked.args = {
+    children,
+    groupName: 'radioGroupUnchecked',
+};
